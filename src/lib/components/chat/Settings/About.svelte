2625a935bce492c09e0259aa216cce721fe91316
Gabriel Susai <gsusai@bateswhite.com>
2024-12-11 14:33:20 -0500

    reverted the changes

--- src/lib/components/chat/Settings/About.svelte
+++ src/lib/components/chat/Settings/About.svelte
@@ -43,7 +43,7 @@
 </script>
 
 <div class="flex flex-col h-full justify-between space-y-3 text-sm mb-6">
-	<div class=" space-y-3">
+	<div class=" space-y-3 overflow-y-scroll max-h-[28rem] lg:max-h-full">
 		<div>
 			<div class=" mb-2.5 text-sm font-medium flex space-x-2 items-center">
 				<div>
@@ -80,14 +80,14 @@
 					</button>
 				</div>
 
-				<!-- <button
+				<button
 					class=" text-xs px-3 py-1.5 bg-gray-100 hover:bg-gray-200 dark:bg-gray-850 dark:hover:bg-gray-800 transition rounded-lg font-medium"
 					on:click={() => {
 						checkForVersionUpdates();
 					}}
 				>
 					{$i18n.t('Check for updates')}
-				</button> -->
+				</button>
 			</div>
 		</div>
 
@@ -106,7 +106,7 @@
 
 		<hr class=" dark:border-gray-850" />
 
-		<!-- <div class="flex space-x-1">
+		<div class="flex space-x-1">
 			<a href="https://discord.gg/5rJgQTnV4s" target="_blank">
 				<img
 					alt="Discord"
@@ -127,7 +127,7 @@
 					src="https://img.shields.io/github/stars/open-webui/open-webui?style=social&label=Star us on Github"
 				/>
 			</a>
-		</div> -->
+		</div>
 
 		<div class="mt-2 text-xs text-gray-400 dark:text-gray-500">
 			{#if !$WEBUI_NAME.includes('Open WebUI')}
